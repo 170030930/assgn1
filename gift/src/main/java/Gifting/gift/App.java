5b8589e4a588bce613d7a0dcd77d1fc76be500a2
lenovo <lenovo@DESKTOP-NN19MM0>
2020-02-08 16:57:17 +0530

    commit

---
+++ gift/src/main/java/Gifting/gift/App.java
@@ -0,0 +1,13 @@
+package Gifting.gift;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
